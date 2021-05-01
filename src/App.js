7309b30a632980e20edc40802e96832b2e3703f9
SharpScratchy <sharp.scratchy@protonmail.com>
2021-05-02 00:29:07 +0200

    💄 Add AnswerResult to application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Piano from "./Piano";
 import Engine from "./Engine";
+import AnswerResult from "./AnswerResult";
 
 function App() {
   return (
     <div className="App">
       <Piano />
+      <AnswerResult />
     </div>
   );
 }
